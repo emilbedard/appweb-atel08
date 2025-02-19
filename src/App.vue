0c6156b550eb4a3c0ec90e3b92ea623d681122e4
emilbedard <2347542@csfoy.ca>
2025-02-19 11:45:41 -0500

    first commit

---
+++ src/App.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+    import Timeline from './components/Timeline.vue'
+    import "bootstrap/dist/css/bootstrap.min.css"
+    import "bootstrap"
+</script>
+
+<template>
+    <div class="container">
+        <Timeline />
+    </div>
+</template>
+
+<style scoped>
+</style>
